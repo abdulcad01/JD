d349dbbcca1dba33ccb5628f17336f7e9036ffb5
abdulcad01 <abdulrahmancad01>
2024-01-05 14:38:01 +0530

    added

---
+++ demo.js
@@ -0,0 +1,15 @@
+console.log('Abbu dah...');
+console.log(10);
+console.log({mine:"abbu",Age:19});
+console.table({mine:"abbu",Age:19});
+console.log([1,2,34,5,6]);
+console.table([1,2,34,5,6]);
+
+var a=5;
+let b = 7;
+const c =6;
+console.log(a);
+console.log(b);
+console.log(c);
+
+a=([1,2,3,4,5])
