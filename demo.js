6dd519ea9f0d79081082f8b1d118e6e97c9b2968
abdulcad01 <abdulrahmancad01>
2024-01-10 15:25:30 +0530

    js file

--- demo.js
+++ demo.js
@@ -31,3 +31,99 @@ m=100;
 var score=(m>=90 && m<=100)? "Distinction" : (m>=70 && m<=89)? 
 "Grade a" : (m>50 && m<=69)?"grade b": (m>35 && m<=49)?"grade c" : "fail"
 console.log(score);
+console.clear();
+//if else
+
+
+
+function marks(m)
+{
+    if(m>=35 && m<=50)
+    {
+    console.log("Just Pass");
+    }
+    else 
+    {
+        console.log("f");
+    }
+     
+    
+}
+marks(50);
+console.clear();
+//nested if else
+function abbu(m)
+{
+    if (m>=0 && m<=34)
+    {
+        console.log("Reappear");
+    }
+   else if(m>=35 && m<=50)
+   {
+    console.log("Grade C");
+   }
+   else if (m>=51 && m<=75)
+   {
+    console.log("Grade B");
+   }
+   else if (m>=76 && m<=89)
+   {
+    console.log("Grade A");
+   }
+   else if (m>=90 && m<=100)
+   {
+    console.log("Distinction");
+   }
+   else {
+    console.log("Enter the mark");
+   }
+}
+abbu(1);
+console.clear();
+//switch
+
+function switchcase(m)
+{
+ switch(m){
+ case 1:
+    console.log("leo");
+    break;
+
+    case 2 :
+        console.log("Thunivu");
+        break;
+
+        case 3 :
+            console.log("Jailer");
+            break;
+
+            case 4 :
+                console.log("Vikram");
+                break;
+
+                case 5 :
+                    console.log("Etharkum Thunindhavan");
+                    break;
+
+                    case 6 :
+                        console.log("Japan");
+                        break;
+
+        default:
+            console.log("Enter the actor name correctly");
+ }
+}
+switchcase(10);
+console.clear();
+//prompt
+function prom(peyar)
+{
+    var names = prompt("Enter first number:");
+    
+
+
+
+
+    console.log( "You gonna have" + names);
+
+prom();
